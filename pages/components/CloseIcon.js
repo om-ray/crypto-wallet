ea8daa52a21eb80a23bcc232bd2f9c63a87ad7b0
123om123 <omihridesh@gmail.com>
2022-03-16 00:31:10 +0530

    added acount details pane

---
+++ pages/components/CloseIcon.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function CloseIcon() {
+  return (
+    <div>
+      <svg width="22" height="22" viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <circle cx="11" cy="11" r="10.5" stroke="#989A9B" />
+        <path d="M7 7L15 15" stroke="#989A9B" />
+        <path d="M15 7L7 15" stroke="#989A9B" />
+      </svg>
+    </div>
+  );
+}
+
+export default CloseIcon;
--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,10 @@ import Web3 from "web3";
 import { useCallback, useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0";
 import { useEffect } from "react";
+import CopyAddressIcon from "./components/CopyAddressIcon";
+import CloseIcon from "./components/CloseIcon";
+import ClipboardJS from "clipboard";
+import { getEthPriceNow } from "get-eth-price";
 
 let networkProviders = {
   "Eth Mainnet": "https://speedy-nodes-nyc.moralis.io/1c58af41ad51021daa7433bb/eth/mainnet",
@@ -23,6 +27,26 @@ function getKeyByValue(object, value) {
   return Object.keys(object).find((key) => object[key] === value);
 }
 
+function groupBy(arr, property) {
+  return arr?.reduce(function (memo, x) {
+    if (!memo[x[property]]) {
+      memo[x[property]] = [];
+    }
+    memo[x[property]].push(x);
+    return memo;
+  }, {});
+}
+
+let convertToUSD = async function (ETH) {
+  let test;
+  let balUSD;
+  await getEthPriceNow("USD").then((data) => {
+    test = Object.keys(data)[0];
+    balUSD = data[test].ETH.USD * ETH;
+  });
+  return balUSD.toFixed(2);
+};
+
 export default function Home() {
   let { user, error, isLoading } = useUser();
   let [networkProvider, setNetworkProvider] = useState(() => {
@@ -37,6 +61,14 @@ export default function Home() {
   });
   let [networkProviderChanged, setNetworkProviderChanged] = useState(false);
   let [encryptedWallet, setEncryptedWallet] = useState(() => {});
+  let [NFTS, setNFTS] = useState(() => null);
+  let [coins, setCoins] = useState(() => null);
+  let [balance, setBalance] = useState(() => null);
+  let [USDBalance, setUSDBalance] = useState(() => null);
+  let [address, setAddress] = useState(() => null);
+  let [showingAccountDetails, setShowingAccountDetails] = useState(() => false);
+  let [showCoins, setShowCoins] = useState(() => true);
+  let [showNFTs, setShowNFTs] = useState(() => false);
 
   useEffect(() => {
     if (user) {
@@ -155,17 +187,78 @@ export default function Home() {
   let addNewAddress = () => {
     let name = window.prompt("Enter the name you would like this account to have");
     web3.eth.accounts.wallet.create(1);
-    addressNamesObject[web3.eth.accounts.wallet[web3.eth.accounts.wallet.length - 1].address] = name;
+    addressNamesObject[web3.eth.accounts.wallet[web3.eth.accounts.wallet.length - 1].address] = name
+      ? name
+      : web3.eth.accounts.wallet[web3.eth.accounts.wallet.length - 1].index + 1;
     console.log(addressNamesObject);
     setUserWallet(Object.entries(web3.eth.accounts.wallet));
     web3.eth.accounts.wallet.save(user.name, `${networkProvider} wallet`);
     createEncryptedWalletObject();
   };
 
+  let openAccountDetails = async (address) => {
+    await fetch(
+      `https://deep-index.moralis.io/api/v2/${address}/nft?chain=eth&format=decimal&order=token_address.ASC`,
+      {
+        method: "GET",
+        headers: {
+          "content-type": "application/json; charset=utf-8",
+          "x-api-key": "Q3Zg3JYiD2uaEbeyeVtHVOfdQDN2ERvqqVX7M15HHa2kXq1uBIy1BpM9hk918OLV",
+        },
+      }
+    ).then((nfts) => {
+      nfts.json().then((nfts) => {
+        nfts = groupBy(nfts.result, "name");
+        nfts = Object.entries(nfts);
+        if (nfts) {
+          console.log(nfts);
+          setNFTS(nfts);
+        }
+        console.log("nfts found");
+      });
+    });
+
+    await fetch(`https://deep-index.moralis.io/api/v2/${address}/erc20?chain=eth`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+        "x-api-key": "Q3Zg3JYiD2uaEbeyeVtHVOfdQDN2ERvqqVX7M15HHa2kXq1uBIy1BpM9hk918OLV",
+      },
+    }).then((coins) => {
+      coins.json().then((coins) => {
+        if (coins) {
+          console.log(coins);
+          setCoins(coins);
+        }
+        console.log("coins found");
+      });
+    });
+
+    await fetch(`https://deep-index.moralis.io/api/v2/${address}/balance?chain=eth`, {
+      method: "GET",
+      headers: {
+        "content-type": "application/json; charset=utf-8",
+        "x-api-key": "Q3Zg3JYiD2uaEbeyeVtHVOfdQDN2ERvqqVX7M15HHa2kXq1uBIy1BpM9hk918OLV",
+      },
+    }).then((balance) => {
+      balance.json().then(async (balance) => {
+        if (balance) {
+          console.log(balance.balance);
+          setBalance(balance.balance);
+          setUSDBalance(await convertToUSD(balance.balance));
+        }
+        console.log("balance found");
+      });
+    });
+
+    setAddress(address);
+    setShowingAccountDetails(true);
+  };
+
   return (
     <div className={styles.container}>
       <div style={{ position: "absolute", margin: "0px auto", top: "60px" }} className={styles.title}>
-        MetaGate
+        OMNI-VERSE
       </div>
       {loggedIn ? (
         <div>
@@ -217,20 +310,281 @@ export default function Home() {
                       return null;
                     }
                     return (
-                      <div style={{ width: "95%" }} className={styles.card}>
+                      <div
+                        onClick={() => {
+                          openAccountDetails(account[1].address);
+                        }}
+                        style={{ width: "95%" }}
+                        className={styles.card}>
                         <h2>
                           {addressNamesObject[account[1].address]
                             ? addressNamesObject[account[1].address]
                             : JSON.parse(account[0]) + 1}
                           <i>:</i>
                         </h2>
-                        <p>Public address: {JSON.stringify(account[1].address)}</p>
-                        <p>Coins: {JSON.stringify(account[1].address)}</p>
+                        <p>Public address: {account[1].address}</p>
                       </div>
                     );
                   })}
                 </div>
               )}
+              {showingAccountDetails ? (
+                <div
+                  style={{
+                    position: "absolute",
+                    top: "0",
+                    left: "0",
+                    width: "100%",
+                    height: "100%",
+                    backgroundColor: "rgba(0, 0, 0, 0.4)",
+                  }}>
+                  <div
+                    style={{
+                      position: "absolute",
+                      top: "15%",
+                      width: "70%",
+                      left: "15%",
+                      height: "70%",
+                      backgroundColor: "white",
+                    }}
+                    className={styles.accountContainer}>
+                    <div
+                      style={{
+                        display: "flex",
+                        flexDirection: "row",
+                        alignItems: "baseline",
+                        borderBottom: "1px solid #eaeaea",
+                        margin: "0px 20px",
+                      }}>
+                      <h1
+                        style={{
+                          margin: "0px 20px 10px 0px",
+                          width: "fit-content",
+                          fontSize: "40px",
+                          fontWeight: "400",
+                        }}>
+                        {addressNamesObject[address]}
+                      </h1>
+                      <button
+                        style={{ width: "120px", display: "flex" }}
+                        data-clipboard-text={address}
+                        className="copy"
+                        onClick={() => {
+                          new ClipboardJS(".copy");
+                          window.alert("Address copied to clipboard");
+                        }}>
+                        <p
+                          style={{
+                            textOverflow: "ellipsis",
+                            height: "fit-content",
+                            width: "80px",
+                            overflow: "auto",
+                            fontSize: "12px",
+                            fontWeight: "300",
+                            margin: "0px 5px 0px 0px",
+                            color: "#989a9b",
+                          }}>
+                          {address}
+                        </p>
+                        <CopyAddressIcon></CopyAddressIcon>
+                      </button>
+                      <div
+                        onClick={() => {
+                          setShowingAccountDetails(false);
+                        }}
+                        style={{ position: "absolute", top: "10px", right: "10px", cursor: "pointer" }}>
+                        <CloseIcon></CloseIcon>
+                      </div>
+                    </div>
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-evenly",
+                        width: "15%",
+                        margin: "0px 0px -15px 10px",
+                        fontFamily: "Playfair display",
+                        fontWeight: "400",
+                      }}>
+                      <button
+                        onClick={() => {
+                          setShowCoins(true);
+                          setShowNFTs(false);
+                        }}
+                        style={
+                          showCoins
+                            ? {
+                                background: "#0077FF",
+                                border: "1px solid #0077FF",
+                                width: "60px",
+                                height: "30px",
+                                margin: "10px 0px 10px 0px",
+                                borderRadius: "5px",
+                                color: "white",
+                              }
+                            : {
+                                background: "#fff",
+                                border: "none",
+                                width: "60px",
+                                height: "30px",
+                                margin: "10px 0px 10px 0px",
+                                borderRadius: "5px",
+                                color: "black",
+                              }
+                        }>
+                        Coins
+                      </button>
+                      <button
+                        onClick={() => {
+                          setShowNFTs(true);
+                          setShowCoins(false);
+                        }}
+                        style={
+                          showNFTs
+                            ? {
+                                background: "#0077FF",
+                                border: "1px solid #0077FF",
+                                width: "60px",
+                                height: "30px",
+                                margin: "10px 0px 10px 0px",
+                                borderRadius: "5px",
+                                color: "white",
+                              }
+                            : {
+                                background: "#fff",
+                                border: "none",
+                                width: "60px",
+                                height: "30px",
+                                margin: "10px 0px 10px 0px",
+                                borderRadius: "5px",
+                                color: "black",
+                              }
+                        }>
+                        NFT's
+                      </button>
+                    </div>
+                    <div style={{ position: "relative", width: "100%", height: "100%", display: "flex" }}>
+                      <div style={{ height: "84%", width: "70%" }}>
+                        <div style={{ height: "100%" }} className={styles.accountContainer}>
+                          {showCoins
+                            ? coins.map((coin) => {
+                                return <div className={styles.card}>{coin.name}</div>;
+                              })
+                            : null}
+                          {showNFTs
+                            ? NFTS.map((nft) => {
+                                return <div className={styles.card}>{nft.name}</div>;
+                              })
+                            : null}
+                        </div>
+                      </div>
+                      <div style={{ display: "flex", flexDirection: "column" }}>
+                        <div
+                          style={{
+                            width: "131%",
+                            height: "20%",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            marginBottom: "5px",
+                            padding: "20px",
+                          }}
+                          className={styles.card}>
+                          <h3
+                            style={{
+                              fontFamily: "Playfair display",
+                              margin: "-5px 0px 5px 0px",
+                              borderBottom: "1px solid #eaeaea",
+                              width: "70%",
+                              textAlign: "center",
+                              padding: "0px 0px 15px 0px",
+                            }}>
+                            Balance
+                          </h3>
+                          <p style={{ fontSize: "14px", margin: "5px" }}>
+                            {balance} <b style={{ fontFamily: "Playfair display" }}>ETH</b>
+                          </p>
+                          <p style={{ fontSize: "14px", margin: "5px" }}>
+                            $ {USDBalance} <b style={{ fontFamily: "Playfair display" }}>USD</b>
+                          </p>
+                        </div>
+                        <div
+                          className={styles.card}
+                          style={{
+                            width: "131%",
+                            height: "7%",
+                            display: "flex",
+                            flexDirection: "row",
+                            alignItems: "center",
+                            borderRadius: "10px",
+                            backgroundColor: "#0077ff",
+                            color: "#fff",
+                            justifyContent: "space-evenly",
+                            padding: "0px",
+                            marginBottom: "5px",
+                          }}>
+                          <button
+                            style={{
+                              color: "white",
+                              width: "40%",
+                              fontSize: "15px",
+                              borderRight: "1px solid white",
+                              height: "100%",
+                              fontFamily: "roboto slab",
+                              fontWeight: "300",
+                            }}>
+                            Buy
+                          </button>
+                          <button
+                            style={{
+                              color: "white",
+                              width: "40%",
+                              fontSize: "15px",
+                              height: "100%",
+                              fontFamily: "roboto slab",
+                              fontWeight: "300",
+                            }}>
+                            Send
+                          </button>
+                          <button
+                            style={{
+                              color: "white",
+                              width: "40%",
+                              fontSize: "15px",
+                              borderLeft: "1px solid white",
+                              height: "100%",
+                              fontFamily: "roboto slab",
+                              fontWeight: "300",
+                            }}>
+                            Swap
+                          </button>
+                        </div>
+                        <div
+                          style={{
+                            width: "131%",
+                            height: "51.5%",
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            padding: "20px",
+                          }}
+                          className={styles.card}>
+                          <h3
+                            style={{
+                              fontFamily: "Playfair display",
+                              margin: "-5px 0px 5px 0px",
+                              borderBottom: "1px solid #eaeaea",
+                              width: "70%",
+                              textAlign: "center",
+                              padding: "0px 0px 15px 0px",
+                            }}>
+                            Transactions
+                          </h3>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              ) : null}
             </div>
           ) : (
             <div
